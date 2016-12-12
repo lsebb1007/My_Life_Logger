9b0c67aa183df9478d3278797b3e6c8ba87580b6
Hwangyonghwan <iamyonghwan@nate.com>
2016-12-12 16:44:58 +0900

    start button click(show time)

--- My_Life_Logger/app/src/main/java/com/example/iamyonghwan/my_life_logger/MapActivity.java
+++ My_Life_Logger/app/src/main/java/com/example/iamyonghwan/my_life_logger/MapActivity.java
@@ -1,16 +1,43 @@
 package com.example.iamyonghwan.my_life_logger;
 
-/**
- * Created by iamyonghwan on 2016-11-24.
- */
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MapActivity extends AppCompatActivity {
-
+    private GpsInfo gps;
+    double latitude;
+    double longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        Button button01 = (Button) findViewById(R.id.button01);
+
+
+
+        button01.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                gps = new GpsInfo(MapActivity.this);
+                // GPS 사용유무 가져오기
+                if (gps.isGetLocation()) {
+
+                    latitude = gps.getLatitude();
+                    longitude = gps.getLongitude();
+
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "당신의 위치 - \n위도: " + latitude + "\n경도: " + longitude,
+                            Toast.LENGTH_LONG).show();
+
+                } else {
+                    // GPS 를 사용할수 없으므로
+                    gps.showSettingsAlert();
+                }
+            }
+        });
     }
 }
--- My_Life_Logger/app/src/main/java/com/example/iamyonghwan/my_life_logger/NoteActivity.java
+++ My_Life_Logger/app/src/main/java/com/example/iamyonghwan/my_life_logger/NoteActivity.java
@@ -1,18 +1,27 @@
 package com.example.iamyonghwan.my_life_logger;
 
-/**
- * Created by iamyonghwan on 2016-11-24.
- */
-
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by iamyonghwan on 2016-11-24.
+ */
+
+
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -27,7 +36,24 @@ public class NoteActivity extends AppCompatActivity {
         imgview = (ImageView) findViewById(R.id.imageView);
         ImageButton buttonCamera = (ImageButton) findViewById(R.id.btn_take_camera);
         ImageButton buttonGallery = (ImageButton) findViewById(R.id.btn_select_gallery);
+        Button buttonstart = (Button) findViewById(R.id.button9) ;
+        Button buttonstop = (Button) findViewById(R.id.button10) ;
+        final TextView textView1 = (TextView) findViewById(R.id.text3);
+        final TextView textView2 = (TextView) findViewById(R.id.text4);
 
+
+        buttonstart.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView1.setText(getDateString()) ;
+            }
+        }) ;
+        buttonstop.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView2.setText(getDateString()) ;
+            }
+        }) ;
         buttonCamera.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
@@ -78,20 +104,33 @@ public class NoteActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode,
                                     int resultCode,
                                     Intent data) {
-        if (requestCode == PICK_FROM_CAMERA) {
-            Bundle extras = data.getExtras();
-            if (extras != null) {
-                Bitmap photo = extras.getParcelable("data");
-                imgview.setImageBitmap(photo);
+        try {
+            if (requestCode == PICK_FROM_CAMERA) {
+                Bundle extras = data.getExtras();
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data");
+                    imgview.setImageBitmap(photo);
+                }
             }
-        }
-        if (requestCode == PICK_FROM_GALLERY) {
-            Bundle extras2 = data.getExtras();
-            if (extras2 != null) {
-                Bitmap photo = extras2.getParcelable("data");
-                imgview.setImageBitmap(photo);
+            if (requestCode == PICK_FROM_GALLERY) {
+                Bundle extras2 = data.getExtras();
+                if (extras2 != null) {
+                    Bitmap photo = extras2.getParcelable("data");
+                    imgview.setImageBitmap(photo);
+                }
             }
+        } catch(NullPointerException e) {
         }
+
     }
+
+    public String getDateString()
+    {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
+        String str_date = df.format(new Date());
+
+        return str_date;
+    }
+
 }
 
