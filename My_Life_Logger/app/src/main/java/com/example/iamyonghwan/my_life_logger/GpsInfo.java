b19afe073af35110dae7c9a1a92ca75380b1dd76
lsebb1007 <js03110@naver.com>
2016-12-12 17:00:06 +0900

    test exam!

--- My_Life_Logger/app/src/main/java/com/example/iamyonghwan/my_life_logger/GpsInfo.java
+++ My_Life_Logger/app/src/main/java/com/example/iamyonghwan/my_life_logger/GpsInfo.java
@@ -16,6 +16,8 @@ import android.os.IBinder;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 
+
+
 public class GpsInfo extends Service implements LocationListener {
 
     private final Context mContext;
